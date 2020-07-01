7ae7f9c91b5ed76a969ccfe7ec7885f666aa93a5
Tim Vaillancourt <timvaillancourt@github.com>
2020-07-02 01:35:26 +0200

    Use heartbeat user

--- sql/setup-heartbeat.sql
+++ sql/setup-heartbeat.sql
@@ -2,4 +2,5 @@ CREATE DATABASE IF NOT EXISTS percona;
 
 CREATE USER IF NOT EXISTS 'heartbeat'@'%';
 SET PASSWORD FOR 'heartbeat'@'%' = PASSWORD('heartbeat');
-GRANT ALL on percona.* TO 'heartbeat'@'%';
+GRANT REPLICATION CLIENT ON *.* TO 'heartbeat'@'%';
+GRANT CREATE, SELECT, INSERT, UPDATE ON percona.* TO 'heartbeat'@'%';
