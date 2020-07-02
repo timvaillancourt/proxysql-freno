ad708fc5562d5bbedad9e02c7247b1f0b3392faa
Tim Vaillancourt <timvaillancourt@github.com>
2020-07-02 03:21:36 +0200

    Use --init-file

--- sql/setup-master.sql
+++ sql/setup-master.sql
@@ -1,5 +1,8 @@
-CREATE DATABASE IF NOT EXISTS percona;
+CREATE USER IF NOT EXISTS 'repl'@'%';
+SET PASSWORD FOR 'repl'@'%' = PASSWORD('repl');
+GRANT REPLICATION SLAVE ON *.* TO 'repl'@'%';
 
+CREATE DATABASE IF NOT EXISTS percona;
 CREATE USER IF NOT EXISTS 'heartbeat'@'%';
 SET PASSWORD FOR 'heartbeat'@'%' = PASSWORD('heartbeat');
 GRANT REPLICATION CLIENT ON *.* TO 'heartbeat'@'%';
