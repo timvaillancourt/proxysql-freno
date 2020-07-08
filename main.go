3b7972348816272fffdbfa6a14fd9cec653a10af
Tim Vaillancourt <timvaillancourt@github.com>
2020-07-08 03:36:09 +0200

    Fix main.go

--- main.go
+++ main.go
@@ -13,60 +13,74 @@ import (
 )
 
 type Row struct {
-	Id       int    `db:"id"`
-	Msg      string `db:"msg"`
-	Hostname string `db:"hostname"`
+	Id  int    `db:"id"`
+	Msg string `db:"msg"`
 }
 
 type App struct {
-	throttled *uint32
+	throttled    uint32
+	throttle_cnt uint32
+	insert_oks   uint32
+	insert_errs  uint32
+	select_oks   uint32
+	select_errs  uint32
+	select_chan  chan int64
 }
 
 func main() {
-	rwDB, err := sqlx.Connect("mysql", "test_rw:123456@tcp(proxysql:3306)/test")
+	db, err := sqlx.Connect("mysql", "testbed_rw:rw123456@tcp(proxysql:3306)/testbed")
+	if err != nil {
+		log.Fatal(err)
+	}
+	dbRO, err := sqlx.Connect("mysql", "testbed_ro:ro123456@tcp(proxysql:3306)/testbed")
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	roDB, err := sqlx.Connect("mysql", "test_ro:123456@tcp(proxysql:3306)/test")
-	if err != nil {
-		log.Fatal(err)
+	log.Printf("ping rw: %v\n", db.Ping())
+	log.Printf("ping ro: %v\n", dbRO.Ping())
+
+	app := &App{
+		select_chan: make(chan int64, 1024),
 	}
+	atomic.StoreUint32(&app.throttled, 0)
 
-	log.Printf("ping rw: %v\n", rwDB.Ping())
-	log.Printf("ping ro: %v\n", roDB.Ping())
-
-	var insert_oks, insert_errs, select_oks, select_errs uint
-	selectChan := make(chan int64, 0)
-	go func(roDB *sqlx.DB, ids chan int64, select_oks, select_errs uint) {
-		for id := range ids {
-			time.Sleep(500 * time.Millisecond)
-			var row Row
-			err = roDB.Get(&row, fmt.Sprintf("SELECT id, msg, @@hostname as hostname FROM test.test WHERE id=%d", id))
-			if err != nil {
-				log.Printf("error SELECTing %d: %v", id, err)
-				select_errs += 1
-			} else {
-				select_oks += 1
-			}
-			log.Printf("select: %v\n", row)
+	go func(dbRO *sqlx.DB, app *App) {
+		for id := range app.select_chan {
+			start := time.Now()
+			go func() {
+				var done bool
+				for !done {
+					select {
+					default:
+						var row Row
+						err = dbRO.Get(&row, fmt.Sprintf("SELECT id, msg FROM testbed.test WHERE id=%d", id))
+						if err != nil {
+							time.Sleep(time.Millisecond)
+						} else {
+							atomic.AddUint32(&app.select_oks, 1)
+							//log.Printf("select: %v, duration: %v", row, time.Since(start))
+							done = true
+						}
+					case <-time.After(500 * time.Millisecond):
+						atomic.AddUint32(&app.select_errs, 1)
+						log.Printf("select timed out at duration: %v", time.Since(start))
+						done = true
+					}
+				}
+			}()
 		}
-	}(roDB, selectChan, select_oks, select_errs)
+	}(dbRO, app)
 
-	go func(insert_oks, insert_errs, select_oks, select_errs uint) {
-		statusTicker := time.NewTicker(3 * time.Second)
+	hc := &http.Client{}
+	frenoTicker := time.NewTicker(2 * time.Second)
+	insertTicker := time.NewTicker(1 * time.Millisecond)
+	statusTicker := time.NewTicker(10 * time.Second)
+	for {
 		select {
 		case <-statusTicker.C:
-			log.Printf("status: insert_ok: %d, insert_err: %d, select_ok: %d, select_errs: %d", insert_oks, insert_errs, select_oks, select_errs)
-		}
-	}(insert_oks, insert_errs, select_oks, select_errs)
-
-	a := &App{throttled: nil}
-	atomic.StoreUint32(a.throttled, 0)
-	go func(a *App) {
-		hc := &http.Client{}
-		frenoTicker := time.NewTicker(time.Second)
-		select {
+			log.Printf("status: insert_ok=%d, insert_err=%d, select_ok=%d, select_errs=%d, throttle_cnt=%d",
+				app.insert_oks, app.insert_errs, app.select_oks, app.select_errs, app.throttle_cnt)
 		case <-frenoTicker.C:
 			res, err := hc.Get("http://freno:8111/check/test/mysql/testbed")
 			if err != nil {
@@ -76,29 +90,26 @@ func main() {
 			bytes, _ := ioutil.ReadAll(res.Body)
 			log.Printf("freno: %s", string(bytes))
 			if res.StatusCode == http.StatusOK {
-				atomic.StoreUint32(a.throttled, 0)
+				atomic.StoreUint32(&app.throttled, 0)
 			} else {
-				atomic.StoreUint32(a.throttled, 1)
+				atomic.StoreUint32(&app.throttled, 1)
 			}
-		}
-	}(a)
-
-	insertTicker := time.NewTicker(250 * time.Millisecond)
-	for {
-		select {
 		case <-insertTicker.C:
-			if atomic.LoadUint32(a.throttled) != 0 {
-				res := rwDB.MustExec("INSERT INTO test.test (msg) values('hello world!')")
-				id, err := res.LastInsertId()
-				if err != nil {
-					insert_errs += 1
-					log.Fatal(err)
-				} else {
-					insert_oks += 1
-					selectChan <- id
-				}
-				log.Printf("insert: %v\n", id)
+			if atomic.LoadUint32(&app.throttled) != 0 {
+				atomic.AddUint32(&app.throttle_cnt, 1)
+				log.Println("inserts throttled")
+				continue
 			}
+			res := db.MustExec("INSERT INTO testbed.test (msg) values('hello world!')")
+			id, err := res.LastInsertId()
+			if err != nil {
+				atomic.AddUint32(&app.insert_errs, 1)
+				log.Fatal(err)
+			} else {
+				atomic.AddUint32(&app.insert_oks, 1)
+				app.select_chan <- id
+			}
+			//log.Printf("insert: %v\n", id)
 		}
 	}
 }
